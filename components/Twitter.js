4449d01fcaa99be6c811f8d9a91f881461d4b707
dev210601 <dev210601@gmail.com>
2023-01-27 13:03:23 +0900

    Add Twitter, TakePicture, RentContent, Market, and My components.

---
+++ components/Twitter.js
@@ -0,0 +1,225 @@
+import React from "react";
+import axios from "axios";
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import IconButton from "@mui/material/IconButton";
+import TextField from "@mui/material/TextField";
+import CircularProgress from "@mui/material/CircularProgress";
+import UploadIcon from "@mui/icons-material/Upload";
+
+import { RBDialog } from "./RealBitsUtil";
+
+// TODO: Check the double uploading.
+const Twitter = ({
+  showTwitterDialog,
+  videoFile,
+  inputImageUrl,
+  inputSetShowTwitterDialog,
+}) => {
+  //----------------------------------------------------------------------------
+  // Variables.
+  //----------------------------------------------------------------------------
+  const TWITTER_DIALOG_WIDTH = 500;
+  const TWITTER_DIALOG_HEIGHT = 400;
+  const uploadDomainRef = React.useRef("");
+  const twitterLoginUrl = React.useRef("");
+
+  const [videoUrl, setVideoUrl] = React.useState();
+  const [imageUrl, setImageUrl] = React.useState("");
+  const [twitterText, setTwitterText] = React.useState("");
+  const [uploadedFilePath, setUploadedFilePath] = React.useState("");
+  const [uploadTwitterEnabled, setUploadTwitterEnabled] = React.useState(false);
+  const [ImageCardMedia, setImageCardMedia] = React.useState(null);
+  const [VideoCardMedia, setVideoCardMedia] = React.useState(null);
+  const [windowWidth, setWindowWidth] = React.useState();
+  const [windowHeight, setWindowHeight] = React.useState();
+
+  React.useEffect(() => {
+    // console.log("call React.useEffect()");
+    // console.log("showTwitterDialog: ", showTwitterDialog);
+    // console.log("videoFile: ", videoFile);
+    // console.log("inputImageUrl: ", inputImageUrl);
+    // console.log("inputSetShowTwitterDialog: ", inputSetShowTwitterDialog);
+    // console.log("window.innerWidth: ", window.innerWidth);
+    // console.log("window.innerHeight: ", window.innerHeight);
+
+    setWindowWidth(window.innerWidth);
+    setWindowHeight(window.innerHeight);
+
+    twitterLoginUrl.current = "/api/twitter/auth";
+    switch (process.env.NEXT_PUBLIC_NETWORK) {
+      case "localhost":
+        uploadDomainRef.current = "http://localhost:3001";
+        break;
+
+      default:
+        uploadDomainRef.current = "https://realbits-staging.herokuapp.com";
+        break;
+    }
+
+    // Use only one of image and video.
+    if (inputImageUrl !== undefined && showTwitterDialog === true) {
+      setImageUrl(inputImageUrl);
+      try {
+        uploadImage({ url: inputImageUrl });
+        setImageCardMedia(
+          <CardMedia
+            component="img"
+            width={(window.innerWidth || 100) / 3}
+            height={(window.innerHeight || 250) / 3}
+            image={inputImageUrl}
+            alt="Preview image"
+          />
+        );
+      } catch (error) {
+        console.error(error);
+      }
+    } else if (videoFile !== undefined && showTwitterDialog === true) {
+      try {
+        uploadVideo();
+        setVideoCardMedia(
+          <CardMedia
+            width={(windowWidth || 100) / 3}
+            height={(windowHeight || 250) / 3}
+            component="video"
+            // autoPlay
+            controls
+            src={videoUrl}
+          />
+        );
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  }, [showTwitterDialog, inputImageUrl, videoFile]);
+
+  const uploadImage = ({ url }) => {
+    const UPLOAD_URL = `${uploadDomainRef.current}/upload_image`;
+    // console.log("url: ", url);
+
+    // https://stackoverflow.com/questions/12168909/blob-from-dataurl
+    fetch(url)
+      .then((res) => {
+        return res.blob();
+      })
+      .then((blob) => {
+        // console.log("blob: ", blob);
+
+        // 2. Set form with image blob data for uploading.
+        const formData = new FormData();
+        formData.append("image_data", blob);
+
+        // 3. Post image blob data with upload url.
+        setUploadTwitterEnabled(false);
+        axios
+          .post(UPLOAD_URL, formData)
+          .then((response) => {
+            // console.log("response: ", response);
+            setUploadedFilePath(response.data);
+            setUploadTwitterEnabled(true);
+          })
+          .catch((error) => {
+            throw error;
+          });
+      });
+  };
+
+  const uploadVideo = () => {
+    // console.log("call uploadVideo()");
+    // console.log("videoFile: ", videoFile);
+
+    const UPLOAD_URL = `${uploadDomainRef.current}/upload_twitter_video`;
+    const formData = new FormData();
+    formData.append("video_data", videoFile);
+
+    setVideoUrl(window.URL.createObjectURL(videoFile));
+    setUploadTwitterEnabled(false);
+
+    axios
+      .post(UPLOAD_URL, formData)
+      .then((response) => {
+        // console.log("response: ", response);
+        setUploadedFilePath(response.data);
+        setUploadTwitterEnabled(true);
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
+
+  // TODO: Set position of twitter dialog
+  // TODO: Set size of twitter dialog.
+  // TODO: Delete file when closing dialog.
+  return (
+    <div>
+      <RBDialog
+        inputOpenRBDialog={showTwitterDialog}
+        inputSetOpenRBDialogFunc={inputSetShowTwitterDialog}
+        inputRBDialogWidth={TWITTER_DIALOG_WIDTH}
+        inputRBDialogHeight={TWITTER_DIALOG_HEIGHT}
+        inputTitle={"Twitter Upload"}
+        inputRight={5}
+        inputTop={5}
+      >
+        <Card>
+          {/*----------------------------------------------------------------*/}
+          {/* Show image or video snapshot.                                  */}
+          {/*----------------------------------------------------------------*/}
+          {ImageCardMedia}
+          {VideoCardMedia}
+
+          {/*----------------------------------------------------------------*/}
+          {/* Show text input.                                               */}
+          {/*----------------------------------------------------------------*/}
+          <CardContent>
+            <TextField
+              fullWidth
+              label="twitter text"
+              id="fullWidth"
+              size="small"
+              placeholder="Write twitter"
+              onChange={(event) => {
+                setTwitterText(event.target.value);
+              }}
+            />
+          </CardContent>
+
+          {/*----------------------------------------------------------------*/}
+          {/* Show upload button.                                            */}
+          {/*----------------------------------------------------------------*/}
+          <CardActions disableSpacing>
+            {uploadTwitterEnabled === true ? (
+              <IconButton
+                aria-label="twitter"
+                onClick={async () => {
+                  const response = await axios.get(
+                    `${twitterLoginUrl.current}?path=${uploadedFilePath}&twitterText=${twitterText}`
+                  );
+                  console.log("response: ", response);
+
+                  if (response.status === 200) {
+                    window.open(response.data.url);
+                  } else {
+                    // TODO: Handle response error.
+                    console.error(response);
+                  }
+
+                  setUploadTwitterEnabled(false);
+                  inputSetShowTwitterDialog(false);
+                }}
+              >
+                <UploadIcon />
+              </IconButton>
+            ) : (
+              <CircularProgress />
+            )}
+          </CardActions>
+        </Card>
+      </RBDialog>
+    </div>
+  );
+};
+
+export default Twitter;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,57 +1,22 @@
 import React from "react";
-
 import AvatarView from "../components/AvatarView";
+import TakePicture from "../components/TakePicture";
+import RentContent from "../components/RentContent";
 
 const Service = () => {
-  //----------------------------------------------------------------------------
-  // Constant variables.
-  //----------------------------------------------------------------------------
-  // "https://cdn.glitch.com/29e07830-2317-4b15-a044-135e73c7f840%2FAshtra.vrm?v=1630342336981";
+  // * -------------------------------------------------------------------------
+  // * Constant variables.
+  // * -------------------------------------------------------------------------
   const [avatarUrl, setAvatarUrl] = React.useState("default.vrm");
 
-  let RENT_MARKET_ADDRESS;
-  let TEST_NFT_ADDRESS;
-  let SERVICE_ADDRESS;
-  let BLOCKCHAIN_NETWORK;
-  // service url : https://realbits-snapshot.s3.ap-northeast-2.amazonaws.com/realbits-snapshot.json
-
-  // Sample collection data.
-  // collectionAddress: 0xE5C46238c2Cf9CD7A36a51274f04958A59daB161
-  // collectionUri: https://js-nft.s3.ap-northeast-2.amazonaws.com/collection.json
-
-  // console.log("App process.env.NETWORK: ", process.env.NETWORK);
-  switch (process.env.NETWORK) {
-    case "localhost":
-    default:
-      RENT_MARKET_ADDRESS = "0xa513E6E4b8f2a923D98304ec87F64353C4D5C853";
-      TEST_NFT_ADDRESS = "0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6";
-      SERVICE_ADDRESS = "0x70997970C51812dc3A010C7d01b50e0d17dc79C8";
-      BLOCKCHAIN_NETWORK = "0x539";
-      break;
-
-    case "matic":
-      RENT_MARKET_ADDRESS = "0x90f20cB6A0665B12c4f2B62796Ff183bF192F55c";
-      TEST_NFT_ADDRESS = "0x82087ff39e079c44b98c3abd053f734b351d5b20";
-      SERVICE_ADDRESS = "0x3851dacd8fA9F3eB64D69151A3597F33E5960A2F";
-      BLOCKCHAIN_NETWORK = "0x137";
-      break;
-
-    case "maticmum":
-      RENT_MARKET_ADDRESS = "0x1b5054C7931b18Ec8E0d5e5F5D0cBD845F3485b8";
-      TEST_NFT_ADDRESS = "0x82087ff39e079c44b98c3abd053f734b351d5b20";
-      SERVICE_ADDRESS = "0x3851dacd8fA9F3eB64D69151A3597F33E5960A2F";
-      BLOCKCHAIN_NETWORK = "0x13881";
-      break;
-  }
-
-  //----------------------------------------------------------------------------
-  // Variable references.
-  //----------------------------------------------------------------------------
+  // * -------------------------------------------------------------------------
+  // * Variable references.
+  // * -------------------------------------------------------------------------
   const rentMarketRef = React.useRef();
 
-  //----------------------------------------------------------------------------
-  // Function references.
-  //----------------------------------------------------------------------------
+  // * -------------------------------------------------------------------------
+  // * Function references.
+  // * -------------------------------------------------------------------------
   const getImageDataUrl = React.useRef();
   const getMediaStreamFuncRef = React.useRef();
   const setTransformAvatarFuncRef = React.useRef();
@@ -68,21 +33,32 @@ const Service = () => {
   const setBackgroundVideoFuncRef = React.useRef();
   const stopScreenEventFuncRef = React.useRef();
 
-  //----------------------------------------------------------------------------
-  // Function.
-  //----------------------------------------------------------------------------
-  const selectAvatar = (element: {
+  function selectAvatar(element: {
     metadata: { realbits: { avatar_url: React.SetStateAction<string> } };
-  }) => {
+  }) {
     // console.log("selectAvatar metadata: ", element.metadata);
     setAvatarUrl(element.metadata.realbits.avatar_url);
-  };
+  }
 
   return (
     <>
-      {/*------------------------------------------------------------------*/}
-      {/* AvatarView component.                                            */}
-      {/*------------------------------------------------------------------*/}
+      {/* // * --------------------------------------------------------------*/}
+      {/* // * RentContent component.                                        */}
+      {/* // * --------------------------------------------------------------*/}
+      <RentContent
+        selectAvatarFunc={selectAvatar}
+        rentMarketAddress={process.env.NEXT_PUBLIC_RENT_MARKET_CONTRACT_ADDRESS}
+        blockchainNetwork={process.env.NEXT_PUBLIC_BLOCKCHAIN_NETWORK}
+        testNftAddress={process.env.NEXT_PUBLIC_LOCAL_NFT_CONTRACT_ADDRESS}
+        serviceAddress={process.env.NEXT_PUBLIC_SERVICE_OWNER_ADDRESS}
+        openMyFuncRef={openMyFuncRef}
+        openMarketFuncRef={openMarketFuncRef}
+        rentMarketRef={rentMarketRef}
+      />
+
+      {/* // * --------------------------------------------------------------*/}
+      {/* // * AvatarView component.                                         */}
+      {/* // * --------------------------------------------------------------*/}
       <AvatarView
         gltfDataUrl={avatarUrl}
         getImageDataUrlFunc={getImageDataUrl}
@@ -92,6 +68,15 @@ const Service = () => {
         // VideoChat -> AvatarView call for changing avatar canvas position.
         // ScreenView -> AvatarView call for changing avatar canvas position.
         setTransformAvatarFunc={setTransformAvatarFuncRef}
+      />
+
+      {/* // * --------------------------------------------------------------*/}
+      {/* // * TakePicture component.                                        */}
+      {/* // * --------------------------------------------------------------*/}
+      <TakePicture
+        getImageDataUrlFunc={getImageDataUrl}
+        takePictureFuncRef={takePictureFuncRef}
+        rentMarketRef={rentMarketRef}
       />
     </>
   );
