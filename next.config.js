3b3bd5673681c506732551037787e0b71d82134d
realbits-lab <dev.realbits@gmail.com>
2023-02-03 00:15:13 +0900

    Fix rent market argument name bug.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 const nextConfig = {
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/",
+  //       destination: "/home",
+  //       permanent: false,
+  //     },
+  //   ];
+  // },
   reactStrictMode: false,
   transpilePackages: ["kalidokit", "rent-market"],
 };
