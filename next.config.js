51c9c8905a4e4098e4ea8159ac344da725a3a512
realbits-lab <dev.realbits@gmail.com>
2023-03-29 13:45:19 +0900

    Add webpackbar and lint avoid list.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
-const nextConfig = {
+const withProgressBar = require("next-progressbar");
+const WebpackBar = require("webpackbar");
+
+const nextConfig = withProgressBar({
   reactStrictMode: false,
   // transpilePackages: ["kalidokit", "rent-market", "v3d-core-realbits"],
-  transpilePackages: ["kalidokit", "rent-market"],
-};
+  // transpilePackages: ["kalidokit", "rent-market"],
+  transpilePackages: ["kalidokit"],
+  webpack: (config) => {
+    config.plugins.push(
+      new WebpackBar({
+        fancy: true,
+        profile: true,
+        basic: false,
+      })
+    );
+
+    return config;
+  },
+});
 
 module.exports = nextConfig;
