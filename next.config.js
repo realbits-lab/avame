d56409e6e0a257ecf37ca74dfe6725fe2288cc95
realbits-lab <dev.realbits@gmail.com>
2023-03-29 21:59:38 +0900

    Ignore lint.

--- next.config.js
+++ next.config.js
@@ -2,9 +2,9 @@ const withProgressBar = require("next-progressbar");
 const WebpackBar = require("webpackbar");
 
 const nextConfig = withProgressBar({
-  // eslint: {
-  //   ignoreDuringBuilds: true,
-  // },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   reactStrictMode: false,
   // transpilePackages: ["kalidokit", "rent-market"],
   transpilePackages: ["kalidokit"],
