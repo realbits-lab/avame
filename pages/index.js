2d6cc1bbf99fbf0e287bd0c37d5a70643d0873d6
realbits-lab <dev.realbits@gmail.com>
2023-05-17 16:59:51 +0900

    Update v3d package version.

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ const Service = () => {
   //* Constant variables.
   //*---------------------------------------------------------------------------
   //* TODO: Change later.
-  // const DEFAULT_MODEL_PATH = "default.vrm";
+  // const DEFAULT_MODEL_PATH = "1.vrm";
   const DEFAULT_MODEL_PATH =
     "https://dulls-nft.s3.ap-northeast-2.amazonaws.com/vrm/1.vrm";
   const [avatarUrl, setAvatarUrl] = React.useState(DEFAULT_MODEL_PATH);
